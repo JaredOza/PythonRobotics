0c9211a6f6fbd069a87ab59e5e76ab8d805a6a04
PYBrulin <pierre-yves.brulin@hexadrone.fr>
2021-07-05 11:54:06 +0200

    Rotation matrix correction

--- AerialNavigation/drone_3d_trajectory_following/drone_3d_trajectory_following.py
+++ AerialNavigation/drone_3d_trajectory_following/drone_3d_trajectory_following.py
@@ -184,7 +184,7 @@ def rotation_matrix(roll, pitch, yaw):
         [[cos(yaw) * cos(pitch), -sin(yaw) * cos(roll) + cos(yaw) * sin(pitch) * sin(roll), sin(yaw) * sin(roll) + cos(yaw) * sin(pitch) * cos(roll)],
          [sin(yaw) * cos(pitch), cos(yaw) * cos(roll) + sin(yaw) * sin(pitch) *
           sin(roll), -cos(yaw) * sin(roll) + sin(yaw) * sin(pitch) * cos(roll)],
-         [-sin(pitch), cos(pitch) * sin(roll), cos(pitch) * cos(yaw)]
+         [-sin(pitch), cos(pitch) * sin(roll), cos(pitch) * cos(roll)]
          ])
 
 
